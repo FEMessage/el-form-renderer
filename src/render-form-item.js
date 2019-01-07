1e1f4c37124da162eb9f68a914d65b8845ff26b8
Alvin <Alvin-Liu@users.noreply.github.com>
2019-01-07 10:40:54 +0800

    feature: trim处理字符串两端空格 (#7)
    * 添加字符串去空格
    
    * README添加对应的trim说明
    
    * example 添加 trim 示例
    
    * 修改 trim 为在对应组件执行 input 事件时触发
    
    * 默认 string 类型去空格
    
    * 示例代码去掉 trim: true
    
    * Update README.md

--- src/render-form-item.js
+++ src/render-form-item.js
@@ -67,7 +67,9 @@ export default {
         on: {
           // 手动更新表单数据
           input: (value) => {
-            this.$emit('updateValue', { id: data.$id, value })
+            // 默认字符串类型处理去空格
+            const trimVal = typeof value === 'string' && (data.trim === undefined || data.trim) ? value.trim() : value
+            this.$emit('updateValue', { id: data.$id, value: trimVal })
           }
         }
       }, [
