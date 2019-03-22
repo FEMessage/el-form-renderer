fb060df169ce109d3d53bfcfa0df4d8707b35c08
evillt <ijoec123@gmail.com>
2019-03-23 01:58:49 +0800

    feat: use vue template

---
+++ src/index.vue
@@ -0,0 +1,211 @@
+<template>
+  <el-form ref="elForm" :model="value" v-bind="$props">
+    <template v-for="(item, k) in content">
+      <slot :name="`$id:${item.$id}`"></slot>
+
+      <component
+        :is="item.$type === group
+          ? 'RenderFormGroup'
+          : 'RenderFormItem'"
+        :key="k"
+        v-bind="resolveProps(item)"
+        @updateValue="updateValue"
+      ></component>
+    </template>
+
+    <slot></slot>
+  </el-form>
+</template>
+
+<script>
+import RenderFormItem from './render-form-item'
+import RenderFormGroup from './render-form-group'
+import Form from 'element-ui/lib/form'
+import _set from 'lodash.set'
+import { isObject } from './utils'
+
+const group = 'group'
+
+function clone(data) {
+  if (Array.isArray(data)) {
+    return data.map(clone)
+  } else if (data && typeof data === 'object') {
+    let obj = Object.assign({}, data)
+    for (let key in obj) {
+      if (!obj.hasOwnProperty(key)) continue
+      if (typeof obj[key] === 'object') {
+        obj[key] = clone(obj[key])
+      }
+    }
+    return obj
+  } else {
+    return data
+  }
+}
+
+export default {
+  name: 'ElFormRenderer',
+
+  components: {
+    RenderFormItem,
+    RenderFormGroup
+  },
+
+  props: Object.assign(
+    {},
+    Form.props,
+    {
+      content: {
+        type: Array,
+        required: true
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ),
+
+  beforeMount() {
+    this.content.forEach(this.initItemValue)
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      Object.keys(Form.methods).forEach(item => {
+        this[item] = this.$refs.elForm[item]
+      })
+    })
+  },
+
+  data() {
+    return {
+      value: {},
+      options: this.content.reduce((con, item) => {
+        con[item.$id] = item.$type === group
+          ? item.$items.reduce((acc, cur) => {
+            acc[cur.$id] = cur.$options || []
+            return acc
+          }, {})
+          : (item.$options || [])
+        return con
+      }, {}),
+      group
+    }
+  },
+
+  watch: {
+    content: {
+      handler(val) {
+        if (!val.length) return
+        this.updateOptions(val)
+      },
+      deep: true
+    }
+  },
+
+  methods: {
+    initItemValue(item) {
+      if (!item.$id || this.value[item.$id] !== undefined) return
+      let defaultVal
+      if (item.$type === group) {
+        // group
+        defaultVal = item.$items.reduce((acc, cur) => {
+          cur.$default && cur.$id && (acc[cur.$id] = cur.$default)
+          return acc
+        }, {})
+      } else if (item.$default !== undefined) {
+        // not group
+        defaultVal = item.$default
+      }
+      defaultVal !== undefined && this.updateValue({ id: item.$id, value: defaultVal })
+    },
+
+    getFormValue() {
+      const getValue = (values, content) => {
+        return Object.keys(values).reduce((acc, key) => {
+          const item = content.find(it => it.$id === key)
+          if (!item) {
+            return acc
+          }
+
+          // 如果类型是group，对值递归处理
+          if (item.$type === group) {
+            acc[key] = getValue(values[key], item.$items)
+          } else {
+            if (item.outputFormat) {
+              const formatVal = item.outputFormat(clone(values[key]))
+              // 如果 outputFormat 返回的是一个对象，则合并该对象，否则在原有 acc 上新增该 属性：值
+              isObject(formatVal) ? Object.assign(acc, formatVal) : (acc[key] = formatVal)
+            } else {
+              acc[key] = clone(values[key])
+            }
+          }
+
+          return acc
+        }, {})
+      }
+      return getValue(this.value, this.content)
+    },
+
+    setOptions(id, options) {
+      if (!id || !Array.isArray(options)) return
+      _set(this.options, id, options)
+    },
+
+    updateForm (values) {
+      const updateValue = content => {
+        return content.reduce((acc, item) => {
+          const value = item.$type === group
+            ? updateValue(item.$items)
+            : (item.inputFormat && item.inputFormat(values) || values[item.$id])
+
+          if (value !== undefined) {
+            _set(acc, item.$id, value)
+          }
+
+          return acc
+        }, {})
+      }
+
+      this.value = Object.assign({}, this.value, updateValue(this.content))
+    },
+
+    updateValue({ id, value }) {
+      this.value = Object.assign({}, this.value, {
+        [id]: value
+      })
+    },
+
+    updateOptions(content) {
+      const { options } = this
+
+      content.forEach(item => {
+        if (item.$type !== group && options[item.$id]) {
+          return
+        }
+        if (item.$type === group) {
+          // 如果该group是动态添加的，则为 options 重新初始化属性
+          if (options[item.$id] === undefined) {
+            this.$set(options, item.$id, {})
+          }
+          this.updateOptions(item.$items, options[item.$id])
+          return
+        }
+        this.$set(options, item.$id, item.$options || [])
+      })
+    },
+
+    resolveProps(item) {
+      return {
+        data: item,
+        value: this.value,
+        itemValue: this.value[item.$id],
+        disabled: this.disabled,
+        options: this.options[item.$id],
+        _parent: this
+      }
+    }
+  }
+}
+</script>
