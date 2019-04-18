2436bcfdd9f3298497c6ae6a9746423a63d640b3
evillt <ijoec123@gmail.com>
2019-04-18 15:07:55 +0800

    refactor: cli v1

---
+++ styleguide.config.js
@@ -0,0 +1,58 @@
+const {VueLoaderPlugin} = require('vue-loader')
+const path = require('path')
+const glob = require('glob')
+
+const demos = glob.sync('docs/!(basic).md')
+const demoSections = demos.map(filePath => ({
+  name: path.basename(filePath, '.md'),
+  content: filePath
+}))
+
+module.exports = {
+  styleguideDir: 'docs',
+  pagePerSection: true,
+  ribbon: {
+    url: 'https://github.com/FEMessage/el-form-renderer'
+  },
+  sections: [
+    {
+      name: 'Components',
+      components: 'src/index.js',
+      usageMode: 'expand'
+    },
+    {
+      name: 'Demo',
+      content: 'docs/basic.md',
+      sections: demoSections
+    }
+  ],
+  require: ['./config/element.js', './config/upload-to-ali.js'],
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.styl(us)?$/,
+          loaders: ['vue-style-loader', 'css-loader', 'stylus-loader']
+        },
+        {
+          test: /\.(woff2?|eot|[to]tf)(\?.*)?$/,
+          loader: 'file-loader'
+        }
+      ]
+    },
+    plugins: [new VueLoaderPlugin()]
+  }
+}
