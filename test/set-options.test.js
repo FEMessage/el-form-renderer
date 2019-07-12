4160084940a34dcd694e2b0e332433435b7751e5
EVILLT <ijoec123@gmail.com>
2019-07-12 19:24:35 +0800

    fix(setOptions): 修复setOptions

---
+++ test/set-options.test.js
@@ -0,0 +1,48 @@
+import component from '../src/el-form-renderer'
+const {setOptions} = component.methods
+
+const options = {
+  shell: [
+    {
+      value: 'zsh'
+    }
+  ],
+  name: [
+    {
+      value: 'finn hudson'
+    }
+  ]
+}
+
+const mocking = {
+  options,
+  setOptions
+}
+
+test('set options', () => {
+  mocking.setOptions('shell', [
+    {
+      name: 'using fish shell',
+      value: 'fish shell'
+    }
+  ])
+  mocking.setOptions('name', [
+    {
+      value: 'reachel berry'
+    }
+  ])
+
+  expect(mocking.options).toEqual({
+    shell: [
+      {
+        name: 'using fish shell',
+        value: 'fish shell'
+      }
+    ],
+    name: [
+      {
+        value: 'reachel berry'
+      }
+    ]
+  })
+})
