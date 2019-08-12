8338b2f3b04a805445d3ac583e6d1b49301aaa37
evillt <hujiehao@deepexi.com>
2019-08-12 19:40:26 +0800

    fix($core): init-options should before the init-value
    close #95

--- src/el-form-renderer.js
+++ src/el-form-renderer.js
@@ -70,6 +70,7 @@ export default {
   },
   beforeMount() {
     this._content = transformContent(this.content)
+    this.initItemOption()
     this._content.forEach(this.initItemValue)
   },
   mounted() {
@@ -93,8 +94,6 @@ export default {
         })
       }
     })
-
-    this.initItemOption()
   },
   props: Object.assign({}, Form.props, {
     content: {
