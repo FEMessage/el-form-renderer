492cafe17475a9ef7b9ff2cfc7b25b70304e2e09
Donald Shen <825870831@qq.com>
2020-02-24 11:39:33 +0800

    fix: remote 仅在有效变更时发送请求

--- cypress/integration/content.spec.js
+++ cypress/integration/content.spec.js
@@ -8,6 +8,7 @@ describe('测试 content 示例', function() {
   it('基础用例', function() {
     cy.contains('禁用第一项').click()
     cy.$getFormItemInput('name').should('be.disabled')
+    cy.contains('requestRemoteCount: 1')
     cy.$getFormItemInput('region')
       .first()
       .click()
@@ -16,7 +17,9 @@ describe('测试 content 示例', function() {
     cy.contains('更新 region 的 options').click()
     cy.contains('shanghai').should('not.exist')
     cy.contains('广州').should('be.visible')
+    cy.contains('requestRemoteCount: 1') // 更改 content 配置不应重复请求 remote
     cy.contains('随机插入表单项').click()
     cy.contains('表单项1')
+    cy.contains('requestRemoteCount: 1') // 更改 content 配置不应重复请求 remote
   })
 })
