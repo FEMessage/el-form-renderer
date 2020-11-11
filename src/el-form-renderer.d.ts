4c9b3de2322d83d888e84b046e91850e8ac9c6fb
4Ark <gd.4ark@gmail.com>
2020-11-11 04:09:07 -0600

    feat: add types (#200)

---
+++ src/el-form-renderer.d.ts
@@ -0,0 +1,87 @@
+import Vue, {VueConstructor, VNode} from 'vue'
+
+import {Form} from '@femessage/types/element-ui'
+
+declare module '@femessage/el-form-renderer' {
+  class FemessageComponent extends Vue {
+    static install(vue: typeof Vue): void
+  }
+
+  type CombinedVueInstance<
+    Instance extends Vue,
+    Data,
+    Methods,
+    Computed,
+    Props
+  > = Data & Methods & Computed & Props & Instance
+
+  type ExtendedVue<
+    Instance extends Vue,
+    Data,
+    Methods,
+    Computed,
+    Props
+  > = VueConstructor<
+    CombinedVueInstance<Instance, Data, Methods, Computed, Props> & Vue
+  >
+
+  type Combined<Data, Methods, Computed, Props> = Data &
+    Methods &
+    Computed &
+    Props
+
+  type ElFormRendererData = {
+    value: {[key: string]: any}
+    options: {[key: string]: any}
+    initValue: null | {[key: string]: any}
+  }
+
+  type ElFormRendererMethods = {
+    resetFields: () => void
+
+    getFormValue: () => {[key: string]: any}
+
+    updateForm: (newValue: any) => void
+
+    setOptions: (id: string, options: any[]) => void
+  }
+
+  type ElFormRendererComputed = {}
+
+  export interface FormContentItem {
+    id: string
+    label?: string | VNode
+    [key: string]: any
+  }
+
+  export type FormContent = Array<FormContentItem>
+
+  type ElFormRendererProps = {
+    content: FormContent
+    disabled: boolean
+    readonly: boolean
+    form: {[key: string]: any}
+  }
+
+  type ElFormRenderer = Combined<
+    ElFormRendererData,
+    ElFormRendererMethods,
+    ElFormRendererComputed,
+    ElFormRendererProps
+  >
+
+  export interface ElFormRendererType
+    extends FemessageComponent,
+      ElFormRenderer,
+      Form {}
+
+  const ElFormRendererConstruction: ExtendedVue<
+    Vue,
+    ElFormRendererData,
+    ElFormRendererMethods,
+    ElFormRendererComputed,
+    ElFormRendererProps
+  >
+
+  export default ElFormRendererConstruction
+}
