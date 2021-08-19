45819c87e0775cbae26e56d647730ece6c8e729b
levy <897895407@qq.com>
2021-08-19 16:56:26 +0800

    docs: update

--- src/el-form-renderer.vue
+++ src/el-form-renderer.vue
@@ -188,6 +188,8 @@ export default {
       this.value = {...this.value, [id]: value}
     },
     /**
+     * 当 strict 为 true 时，只返回设置的表单项的值, 过滤掉冗余字段, 更多请看 update-form 示例
+     * @param {{strict: Boolean}} 默认 false
      * @return {object} key is item's id, value is item's value
      * @public
      */
